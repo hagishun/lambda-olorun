9a0766431559d3a094bd8df015a238c8226fb7fb
Cristóbal Castillo <cristobal.castillo@consensys.net>
2018-06-28 16:12:53 -0400

    add overwrite blockchains file with contract addressed

--- src/lib/deployContracts.js
+++ src/lib/deployContracts.js
@@ -1,19 +1,41 @@
 const UPortClient = require("uport-client").UPortClient;
 const deploy = require("uport-client").deploy;
-const private_blockchains = require("./private-blockchains");
+const chains = require("./private-blockchains");
+const fs = require("fs");
 
-for (let network in private_blockchains) {
-  if ({}.hasOwnProperty.call(private_blockchains, network)) {
+let jsonContent = {};
+for (let network in chains) {
+  if ({}.hasOwnProperty.call(chains, network)) {
     const uportClient = new UPortClient(network);
     uportClient.initKeys();
 
-    console.log(private_blockchains[network]["rpcUrl"]);
-
-    if (network && network.rpcUrl) {
-      deploy(private_blockchains[network]["rpcUrl"]).then(contracts => {
-        console.log("contracts deployed!");
-        console.log(contracts);
-      });
+    if (chains[network] && chains[network]["rpcUrl"]) {
+      deploy(chains[network]["rpcUrl"])
+        .then(contracts => {
+          chains[network]["uPort"]["IdentityManager"] =
+            contracts.IdentityManager;
+          chains[network]["uPort"]["MetaIdentityManager"] =
+            contracts.MetaIdentityManager;
+          chains[network]["uPort"]["TxRelay"] = contracts.TxRelay;
+          console.log("contracts deployed!");
+        })
+        .catch(err => {
+          console.log("error deploying contracts", err);
+        });
+      jsonContent[network] = chains[network];
     }
   }
 }
+
+fs.writeFile(
+  "./src/lib/private-blockchains.js",
+  "module.exports = " + JSON.stringify(jsonContent),
+  "utf8",
+  function(err) {
+    if (err) {
+      console.log("Error updating private blockchains file", err);
+      return console.log(err);
+    }
+    console.log("Private blockchains file updated");
+  }
+);

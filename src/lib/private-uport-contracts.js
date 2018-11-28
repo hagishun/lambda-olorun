5830a6ebfb207a31f49bdeff6378bd1a259d414b
hagishun <s-hagihira@nri.co.jp>
2018-11-28 11:48:30 +0900

    Update private-uport-contracts.js
    add private network info

--- src/lib/private-uport-contracts.js
+++ src/lib/private-uport-contracts.js
@@ -3,6 +3,11 @@ module.exports = {
         IdentityManager: '0xb8a00506e12d39522cd1787389ae8f83db536e46',
         MetaIdentityManager: '0xd7dc3926bc6089a5be4815215ceaa6e707591023',
         TxRelay: '0x6a841ba0ea1a88cfbc085220fc6b65973afca431'
+    },
+    local: {
+        IdentityManager: '0xde75613b0f6f843b4b618eb2f9a09424a16fd46a',
+        MetaIdentityManager: '0x0f0b92221c76b92c82d16269013bc9b8b7d09350',
+        TxRelay: '0x2b3ebf89e86d66375fbbd981d2089c3690137159'
     }
 }
   
